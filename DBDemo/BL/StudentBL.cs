05a395fa45a3692e668221afdb7d20eb39f597e1
Keyur Adhyaru <adhyarukeyur@gmail.com>
2024-03-02 19:23:07 +0530

    init

---
+++ DBDemo/BL/StudentBL.cs
@@ -0,0 +1,196 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBDemo.BL
+{
+    public class StudentBL
+    {
+        public MySqlConnection _objConect;
+
+        /// <summary>
+        /// Init Object of Database.
+        /// </summary>
+        public StudentBL()
+        {
+            //STEP 1 - Create the Object of MYSQL Connection
+            //_objConect = new MySqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+            _objConect = new MySqlConnection("SERVER=localhost; port=3306; DATABASE=student; username=root; PASSWORD='123456'");
+        }
+
+        /// <summary>
+        /// Get All Students Data
+        /// </summary>
+        /// <returns>datatable</returns>
+        public DataTable GetStudent()
+        {
+            //STEP 2 : Create Query and datatable
+            DataTable studentTbl = new DataTable();
+
+            //Query 
+            string _studentQuery = string.Format(@"SELECT * FROM STD01");
+
+            try
+            {
+                //STEP 4 : Open Connection
+                _objConect.Open();
+
+                //STEP 5: Create Adapter Object
+                using (MySqlDataAdapter da = new MySqlDataAdapter(_studentQuery, _objConect))
+                {
+                    //STEP 6 : Fill data
+                    da.Fill(studentTbl);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                //Close the Connection
+                _objConect.Close();
+            }
+            
+            return studentTbl;
+        }
+
+        /// <summary>
+        /// Insert Data into table
+        /// </summary>
+        /// <param name="studentName">Name of the Student</param>
+        /// <param name="studentEnd">enrollment Number</param>
+        /// <param name="sem">Current Sem</param>
+        /// <returns>String Message</returns>
+        public string InsertStudent(string studentName, string studentEnd, int sem)
+        {
+            int result = 0;
+
+            try
+            {
+                //STEP 2 : Create Command Object 
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    //SETP 3 : Open Connection
+                    _objConect.Open();
+
+                    //STEP 4 : SetUp Query String to Command Text
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = _objConect;
+                    cmd.CommandText = "INSERT INTO STD01(D01F02,D01F03) VALUES(@name, @endrol)";
+
+                    //STEP 5 : Add Parameters to Query.
+                    cmd.Parameters.AddWithValue("@name", studentName);
+                    cmd.Parameters.AddWithValue("@endrol", studentEnd);
+
+                    //STEP 6 : Execute Query.
+                    result = cmd.ExecuteNonQuery();
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _objConect.Close();
+            }
+
+            if (result == 1)
+            {
+                return "User Inserted Successfully";
+            }
+            else
+            {
+                return "User not Inserted";
+            }
+        }
+
+        public string UpdateStudent(int id, string studentName, string studentEnd, int sem)
+        {
+            int result = 0;
+
+            try
+            {
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    _objConect.Open();
+
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = _objConect;
+                    cmd.CommandText = "UPDATE STD01 SET D01F02 = @name,D01F03 = @endrol,D01F04 = @sem WHERE D01F01 = @id";
+
+                    cmd.Parameters.AddWithValue("@name", studentName);
+                    cmd.Parameters.AddWithValue("@endrol", studentEnd);
+                    cmd.Parameters.AddWithValue("@sem", sem);
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    result = cmd.ExecuteNonQuery();
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _objConect.Close();
+            }
+
+            if (result == 1)
+            {
+                return "Student Data Updated Successfully";
+            }
+            else
+            {
+                return "Student Data not Updated";
+            }
+        }
+
+        public string DeleteStudent(int id)
+        {
+            int result = 0;
+
+            try
+            {
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    _objConect.Open();
+
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = _objConect;
+                    cmd.CommandText = "DELETE FROM STD01 WHERE D01F01 = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    result = cmd.ExecuteNonQuery();
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _objConect.Close();
+            }
+
+            if (result == 1)
+            {
+                return "Student Data Deleted Successfully";
+            }
+            else
+            {
+                return "Student Data not Deleted";
+            }
+        }
+    }
+}
---
+++ DBDemo/Controllers/StudentController.cs
@@ -0,0 +1,77 @@
+﻿using DBDemo.BL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        #region Public Members
+        StudentBL objStudentBL = new StudentBL();
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Get All Students
+        /// </summary>
+        /// <returns>Student data</returns>
+        [HttpGet]
+        [Route("api/student/GetAllStudent")]
+        public IActionResult GetAllStudent()
+        {
+            return Ok(JsonConvert.SerializeObject(objStudentBL.GetStudent()) );
+        }
+
+        /// <summary>
+        /// Insert Student Data into table
+        /// </summary>
+        /// <param name="studentName">Name of the Student</param>
+        /// <param name="studentEnd">Enrollment Number</param>
+        /// <param name="sem">Currenct SEM</param>
+        /// <returns>Message</returns>
+        [HttpPost]
+        [Route("api/student/InsertStudnet")]
+        public IActionResult InsertStudent(string studentName, string studentEnd, int sem)
+        {
+            return Ok(JsonConvert.SerializeObject(objStudentBL.InsertStudent(studentName, studentEnd, sem)));
+        }
+
+        /// <summary>
+        /// Update Student Data into table
+        /// </summary>
+        /// <param name="id">Id of Student</param>
+        /// <param name="studentName">Name of the Student</param>
+        /// <param name="studentEnd">Enrollment Number</param>
+        /// <param name="sem">Currenct SEM</param>
+        /// <returns>Message</returns>
+        [HttpPut]
+        [Route("api/student/UpdateStudent")]
+        public IActionResult UpdateStudent(int id, string studentName, string studentEnd, int sem)
+        {
+            return Ok(objStudentBL.UpdateStudent(id, studentName, studentEnd, sem));
+        }
+
+        /// <summary>
+        /// Delete Rec of Given Id
+        /// </summary>
+        /// <param name="id">Student Id</param>
+        /// <returns>Message</returns>
+        [HttpDelete]
+        [Route("api/student/DeleteStudent")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok(objStudentBL.DeleteStudent(id));
+        }
+
+        #endregion
+    }
+}
